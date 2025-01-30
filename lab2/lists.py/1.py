951757a0ec216918e059d63a9291de509f716e7a
Каракат Жасулан <karakatzaslan@MacBook-Air-Karakat.local>
2025-01-31 02:15:04 +0500

    tasks lab_2

---
+++ lab2/lists.py/1.py
@@ -0,0 +1,9 @@
+#Access List Items
+list1 = ["apple", "banana", "cherry"]
+print(list1)
+
+#it can be in any types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
---
+++ lab2/lists.py/6.py
@@ -0,0 +1,12 @@
+#list sort
+girl_gr=["Kakos", "Tomiris", "Akbota","Aishabibi"]
+girl_gr.sort()
+print(girl_gr)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist2 = ["banana", "Orange", "Kiwi", "cherry"]
+thislist2.reverse()
+print(thislist2)
