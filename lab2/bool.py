f931c53895352b2677f3211490f6321f07be5778
Каракат Жасулан <karakatzaslan@MacBook-Air-Karakat.local>
2025-01-31 01:15:44 +0500

    tasks for lab2

---
+++ lab2/bool.py
@@ -0,0 +1,18 @@
+#first example
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#second example
+a = 20
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#sort content,not empty string,any numbers,set,dictionary are true
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
