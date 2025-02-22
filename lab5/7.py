e0d2f1884938ac21fde274cc27195c9091aec8b6
Каракат Жасулан <karakatzaslan@MacBook-Air-Karakat.local>
2025-02-22 16:26:14 +0500

    lab_work5

--- lab5/7.py
+++ lab5/7.py
@@ -1,1 +1,7 @@
+def s_to_(snake_str):
+    words = snake_str.split('_')
+    return words[0] + ''.join(word.title() for word in words[1:])
 
+snake_str="hello_world_example"
+camel=s_to_(snake_str)
+print(camel)
